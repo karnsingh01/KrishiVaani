3a5454227cad290b46b39519d4176470901bc896
Karn singh <karnsinghgolu@gmail.com>
2025-09-18 12:11:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kisanvaani-v2';
+const CACHE_NAME = 'kisanvaani-v3';
 const urlsToCache = ['/', '/index.html', '/style.css', '/app.js'];
 
 self.addEventListener('install', (event) => {
