fb960de2bcd580357e7ca75577b469a7ca04418e
Karn singh <karnsinghgolu@gmail.com>
2025-09-25 00:06:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,29 @@
-const CACHE_NAME = 'kisanvaani-v5';
-const urlsToCache = ['/', '/index.html', '/style.css', '/app.js'];
+const CACHE_NAME = 'kisanvaani-v1';
+const urlsToCache = [
+  '/',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
